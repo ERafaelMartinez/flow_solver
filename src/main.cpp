24e1d66049a151f8a50eb9fac0ad5fc0b7bb69ff
Mohammad Al Kamel <modykamel@gmail.com>
2025-11-08 12:51:37 +0100

    feat: adds settings parser

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,34 @@
 
 #include <iostream>
 #include <cstdlib>
+#include "settings/settings.h"
 
 int main(int argc, char *argv[])
 {
-  // write 5 output files
-  for (int i = 0; i < 5; i++)
+  // if the number of given command line arguments is only 1 (= the program name), print out usage information and exit
+  if (argc == 1)
   {
-    writeParaviewOutput(i);
+    std::cout << "usage: " << argv[0] << " <filename>" << std::endl;
+
+    return EXIT_FAILURE;
   }
+
+  // read in the first argument
+  std::string filename = argv[1];
+
+  std::cout << "Parsing settings..." << std::endl;
+#ifndef NDEBUG
+  // print message
+  std::cout << "filename: \"" << filename << "\"" << std::endl;
+#endif
+
+  SettingsParser settingsParser;
+  Settings settings = settingsParser.loadFromFile(filename);
+  std::cout << "Finished parsing!" << std::endl;
+
+#ifndef NDEBUG
+  std::cout << settings;
+#endif
 
   return EXIT_SUCCESS;
 }
--- src/settings/settings.cpp
+++ src/settings/settings.cpp
@@ -1,26 +1,22 @@
-#include <fstream>   // for file operations
-#include <iostream>  // for cout
+#include "settings.h"
 
-void loadFromFile(std::string filename)
+#include <iostream>
+
+std::ostream &operator<<(std::ostream &os, const Settings &s)
 {
-    // open the file
-    std::ifstream file(filename.c_str(), std::ios::in);
-
-    // check if file is open
-    if (!file.is_open())
-    {
-        std::cout << "Error: Could not open parameter file \"" << filename << "\"." << std::endl;
-        return;
-    }
-
-
-    // loop over each line in the file
-    for (int lineNo = 0; !file.eof(); lineNo++)
-    {
-        std::string line;
-        std::getline(file, line);
-
-        // print the line
-        std::cout << "Line " << lineNo << ": " << line << std::endl;
-    }
+    os << "Settings: " << std::endl
+       << "  physicalSize: " << s.physicalSize[0] << " x " << s.physicalSize[1] << ", nCells: " << s.nCells[0] << " x " << s.nCells[1] << std::endl
+       << "  endTime: " << s.endTime << " s, re: " << s.re << ", g: (" << s.g[0] << "," << s.g[1] << "), tau: " << s.tau << ", maximum dt: " << s.maximumDt << std::endl
+       << "  dirichletBC: " << std::endl
+       << "    bottom: (" << s.dirichletBcBottom[0] << "," << s.dirichletBcBottom[1] << ")" << std::endl
+       << "    top: (" << s.dirichletBcTop[0] << "," << s.dirichletBcTop[1] << ")" << std::endl
+       << "    left: (" << s.dirichletBcLeft[0] << "," << s.dirichletBcLeft[1] << ")" << std::endl
+       << "    right: (" << s.dirichletBcRight[0] << "," << s.dirichletBcRight[1] << ")" << std::endl
+       << "  useDonorCell: " << std::boolalpha << s.useDonorCell << std::endl
+       << "    alpha: " << s.alpha << std::endl
+       << "  pressureSolver: " << s.pressureSolver << std::endl
+       << "    omega: " << s.omega << std::endl
+       << "    epsilon: " << s.epsilon << std::endl
+       << "    maximumNumberOfIterations: " << s.maximumNumberOfIterations << std::endl;
+    return os;
 }
