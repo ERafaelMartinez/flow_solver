08c92e77facddeb0dae94d0f037f54862ba27e17
Lilian Spieler <st173327@stud.uni-stuttgart.de>
2025-11-12 15:26:39 +0100

    implement discretization

---
+++ src/discretization/central_differences.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef CENTRALDIFFERENCES_H
+#define CENTRALDIFFERENCES_H
+
+#include <array>
+#include "discretization.h"  
+#include "staggered_grid.h"
+
+class CentralDifferences : public Discretization 
+{
+public:
+/**
+ * Constructor.
+ * 
+ * \param nCells Number of cells in each dimension.
+ * \param meshWidth Mesh width in each dimension.
+ */
+CentralDifferences(std::array<int,2> nCells, std::array<double,2> meshWidth);
+
+double computeDu2Dx(int i, int j) const override;
+double computeDv2Dy(int i, int j) const override;
+double computeDuvDx(int i, int j) const override;
+double computeDuvDy(int i, int j) const override;
+};
+
+#endif
