6d8b6913f55d50d101dd2b7ff5fd1e2c4b7108d3
st195070 <st195070@stud.uni-stuttgart.de>
2026-01-07 12:18:35 +0100

    Feature/ml mae metric (#19)
    * feat: adds mae metric per channel
    
    * feat: changes rmse metric to be per channel
    
    * feat: uses rmse metric instead of mse metric
    
    ---------
    
    Co-authored-by: Mohammad Al Kamel <modykamel@gmail.com>

--- ml/tester.py
+++ ml/tester.py
@@ -1,5 +1,6 @@
 import logging
 import torch
+from math import sqrt
 from dataclasses import dataclass
 from networks.base_model import BaseModel
 from normalizer import Normalizer
@@ -7,17 +8,20 @@ from normalizer import Normalizer
 
 @dataclass
 class TestResult:
-    loss: float = 0
+    loss_rmse_u: float = 0
+    loss_rmse_v: float = 0
+    loss_mae_u: float = 0
+    loss_mae_v: float = 0
     accuracy: float = 0
 
 
 class Tester():
     def __init__(self,
-                 criterion: torch.nn.modules.loss._Loss,
                  testing_dataloader: torch.utils.data.DataLoader,
                  normalizer: Normalizer,
                  ):
-        self.criterion = criterion
+        self.criterion_mse = torch.nn.MSELoss()
+        self.criterion_mae = torch.nn.L1Loss()
         self.testing_dataloader = testing_dataloader
         self.normalizer = normalizer
         self.logger = logging.getLogger(__name__)
@@ -29,7 +33,10 @@ class Tester():
         network.load_model()
         network.eval()
 
-        running_loss = 0
+        running_loss_mse_u = 0
+        running_loss_mse_v = 0
+        running_loss_mae_u = 0
+        running_loss_mae_v = 0
         correct = 0
         total = 0
         with torch.no_grad():
@@ -38,20 +45,53 @@ class Tester():
                 target = target.float()
 
                 output = network(input)
-                loss = self.criterion(output, target)
-                running_loss += loss.item()
 
-                _, target_denormalized = self.normalizer.denormalize(None, target)
-                _, output_denormalized = self.normalizer.denormalize(None, output)
+                _, target_denormalized = self.normalizer.denormalize(
+                    None, target)
+                _, output_denormalized = self.normalizer.denormalize(
+                    None, output)
+
+                target_denormalized_u = target_denormalized[0, 0, :, :]
+                target_denormalized_v = target_denormalized[0, 1, :, :]
+                output_denormalized_u = output_denormalized[0, 0, :, :]
+                output_denormalized_v = output_denormalized[0, 1, :, :]
+
+                # MSE loss per channel
+                loss_mse_u = self.criterion_mse(output_denormalized_u,
+                                                target_denormalized_u)
+                running_loss_mse_u += loss_mse_u.item()
+
+                loss_mse_v = self.criterion_mse(output_denormalized_v,
+                                                target_denormalized_v)
+                running_loss_mse_v += loss_mse_v.item()
+
+                # MAE loss per channel
+                loss_mae_u = self.criterion_mae(output_denormalized_u,
+                                                target_denormalized_u)
+                running_loss_mae_u += loss_mae_u.item()
+
+                loss_mae_v = self.criterion_mae(output_denormalized_v,
+                                                target_denormalized_v)
+                running_loss_mae_v += loss_mae_v.item()
 
                 predictions = output_denormalized.argmax(dim=1)
                 correct += (predictions == target_denormalized).sum().item()
                 total += target_denormalized.size(0)
 
-        test_loss = running_loss / len(self.testing_dataloader)
+        n_testing_dataset = len(self.testing_dataloader)
+
+        test_loss_rmse_u = sqrt(running_loss_mse_u / n_testing_dataset)
+        test_loss_rmse_v = sqrt(running_loss_mse_v / n_testing_dataset)
+        test_loss_mae_u = running_loss_mae_u / n_testing_dataset
+        test_loss_mae_v = running_loss_mae_v / n_testing_dataset
+
         test_accuracy = correct / total
 
-        result = TestResult(test_loss, test_accuracy)
+        result = TestResult(test_loss_rmse_u,
+                            test_loss_rmse_v,
+                            test_loss_mae_u,
+                            test_loss_mae_v,
+                            test_accuracy)
 
         self.logger.info(f"Finished testing network {network.get_name()} with result\n{result}")
 
@@ -65,8 +105,7 @@ def test_all_models(testing_dataloader: torch.utils.data.DataLoader,
     results = {}
     for network in BaseModel.subclasses:
         net = network(model_export_path)
-        criterion = torch.nn.MSELoss()
-        tester = Tester(criterion, testing_dataloader, normalizer)
+        tester = Tester(testing_dataloader, normalizer)
         result = tester.test(net)
         results[net.get_name()] = result
 
